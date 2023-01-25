fb0e508bc99ef3af9ae34c5c6c4450e98c5a30da
PedroJRGarcia <garciapj85@gmail.com>
2023-01-25 16:59:08 +0100

    commit.pgarcia

--- 04_module_react/04_module_basic/src/pods/list/list.tsx
+++ 04_module_react/04_module_basic/src/pods/list/list.tsx
@@ -4,6 +4,7 @@ import { MyContext } from "../../my-provider";
 import { OrganizationSearch } from "../../organization-search";
 import classes from "./list.styles.css";
 import Pagination from "@mui/material/Pagination";
+import { chunk } from "lodash";
 // import { ReactPagination } from "./pagination";
 
 export interface MemberEntity {
@@ -15,36 +16,25 @@ export interface MemberEntity {
 export const ListPage: React.FC = () => {
   const [members, setMembers] = React.useState<MemberEntity[]>([]);
   const { orgName, setOrgName } = React.useContext(MyContext);
+  const [splitMembers, setSplitMembers] = React.useState([]);
+  const [page, setPage] = React.useState(1);
 
   const handleSearch = (organizationName: string) => {
     fetch(`https://api.github.com/orgs/${organizationName}/members`)
-      .then((response) => response.json())
-      .then((json) => setMembers(json));
+    .then((response) => (response.json()))
+    .then((json) => {
+      setMembers(json)
+      setSplitMembers(chunk(json, 5))
+    });
+  };
+
+  const handleChange = (event, value) => {
+  
   };
 
   React.useEffect(() => {
     handleSearch(orgName);
   }, []);
-
-  // const PaginationControlled: React.FC = () => {
-  //   const [page, setPage] = React.useState(1);
-  //   const handleChange = (event, value) => {
-  //     setPage(value);
-  //   };
-
-  //   return (
-  //     <div>
-  //       <Pagination
-  //         count={5}
-  //         page={page}
-  //         onChange={handleChange}
-  //         variant="outlined"
-  //         color="primary"
-  //         shape="rounded"
-  //       />
-  //     </div>
-  //   );
-  // };
 
   return (
     <>
@@ -59,14 +49,23 @@ export const ListPage: React.FC = () => {
         <span className={classes.header}>Id</span>
         <span className={classes.header}>Name</span>
         {members.map((member) => (
-          <>
-            <img src={member.avatar_url} />
+          <div className={classes.row} key={member.id}>
+            <img src={member.avatar_url} width={ 40 } />
             <span>{member.id}</span>
             <Link to={`/detail/${member.login}`}>{member.login}</Link>
-          </>
+          </div>
         ))}
       </div>
-      {/* <PaginationControlled /> */}
+      <div>
+        <Pagination
+          count={Math.round( (members.length/5) + 1 )}
+          page={page}
+          onChange={handleChange}
+          variant="outlined"
+          color="primary"
+          shape="rounded"
+        />
+      </div>
       <Link to="/detail">Navigate to detail page</Link>
     </>
   );
